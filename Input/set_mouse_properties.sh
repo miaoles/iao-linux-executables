f1993564b6ca51c9bc51b1b327bf8215eded6025
miaoles <iao_mm@pm.me>
2023-01-04 10:40:51 -0500

    january update

--- Input/set_mouse_properties.sh
+++ Input/set_mouse_properties.sh
@@ -4,20 +4,23 @@ sleep 6;
 
 # Set Mouse Properties (Speed and Middle Click Scroll)
 
-DEVICE_NAME="Gaming Mouse"
+DEVICE_NAME="pointer:Kingston HyperX Pulsefire Haste"
 ACCELERATION_SPEED=0.5 #0.0-1.0
 
 # Increase Mouse Acceleration Speed
 #xinput --set-prop "${DEVICE_NAME}" "Evdev Wheel Emulation Button" ${ACCELERATION_SPEED}
 #xinput --set-prop "${DEVICE_NAME}" 'libinput Accel Speed' ${ACCELERATION_SPEED}
+#xset m 40/10 10
+xinput --set-prop "pointer:Kingston HyperX Pulsefire Haste" 'Device Accel Profile' 4
+xset m 10/5 5
 
 # Enable Button Scroll Method
-xinput --set-prop "${DEVICE_NAME}" "Evdev Wheel Emulation" 1
+#xinput --set-prop "${DEVICE_NAME}" "Evdev Wheel Emulation" 1
 #xinput --set-prop "${DEVICE_NAME}" "libinput Scroll Method Enabled" 0, 0, 1
 
 # Sets Front Mouse Button as Button Scrolling Button
-xinput --set-prop "${DEVICE_NAME}" "Evdev Wheel Emulation Button" 9
+#xinput --set-prop "${DEVICE_NAME}" "Evdev Wheel Emulation Button" 9
 #xinput --set-prop "${DEVICE_NAME}" "libinput Button Scrolling Button" 9
 
 # Set Scroll Axes (enable Horizontal Scrolling)
-xinput --set-prop "${DEVICE_NAME}" "Evdev Wheel Emulation Axes" 6, 7, 4, 5
+#xinput --set-prop "${DEVICE_NAME}" "Evdev Wheel Emulation Axes" 6, 7, 4, 5
--- Pulseaudio/create-obs-audio-sinks.sh
+++ Pulseaudio/create-obs-audio-sinks.sh
@@ -2,18 +2,27 @@
 
 # Create PulseAudio combines and sinks for OBS
 
-pactl load-module module-null-sink
-pactl load-module module-null-sink
-pactl load-module module-null-sink
+DEFAULT_OUTPUT=alsa_output.pci-0000_0c_00.4.analog-stereo
 
-pactl load-module module-combine-sink sink_name=music-output-combine slaves=2,3
-pactl load-module module-combine-sink sink_name=game-output-combine slaves=2,4
-pactl load-module module-combine-sink sink_name=video-output-combine slaves=2,5
+pactl load-module module-remap-sink sink_name=personal-audio-remap remix=yes master=$DEFAULT_OUTPUT
+pactl load-module module-null-sink sink_name=music-null-sink
+pactl load-module module-null-sink sink_name=game-null-sink
+pactl load-module module-null-sink sink_name=video-null-sink
+pactl load-module module-combine-sink sink_name=music-output-combine slaves=personal-audio-remap,music-null-sink
+pactl load-module module-combine-sink sink_name=game-output-combine slaves=personal-audio-remap,game-null-sink
+pactl load-module module-combine-sink sink_name=video-output-combine slaves=personal-audio-remap,video-null-sink
 
-pacmd 'update-sink-proplist null device.description="Music Null Sink" '
-pacmd 'update-sink-proplist null.2 device.description="Game Null Sink" '
-pacmd 'update-sink-proplist null.3 device.description="Video Null Sink" '
-
+pacmd 'update-sink-proplist personal-audio-remap device.description="Personal Audio Remap" '
+pacmd 'update-sink-proplist music-null-sink device.description="Music Null Sink" '
+pacmd 'update-sink-proplist game-null-sink device.description="Game Null Sink" '
+pacmd 'update-sink-proplist video-null-sink device.description="Video Null Sink" '
 pacmd 'update-sink-proplist music-output-combine device.description="Music Combined Output" '
 pacmd 'update-sink-proplist game-output-combine device.description="Game Combined Output" '
 pacmd 'update-sink-proplist video-output-combine device.description="Video Combined Output" '
+
+#pactl load-module module-null-sink sink_name=music-null-sink sink_properties=device.description="Music Null Sink"
+#pactl load-module module-null-sink sink_name=game-null-sink sink_properties=device.description="Game Null Sink"
+#pactl load-module module-null-sink sink_name=video-null-sink sink_properties=device.description="Video Null Sink"
+#pactl load-module module-combine-sink sink_name=music-output-combine slaves=personal-audio-remap,music-null-sink sink_properties=device.description="Music Combined Output"
+#pactl load-module module-combine-sink sink_name=game-output-combine slaves=personal-audio-remap,game-null-sink sink_properties=device.description="Game Combined Output"
+#pactl load-module module-combine-sink sink_name=video-output-combine slaves=personal-audio-remap,video-null-sink sink_properties=device.description="Video Combined Output"
